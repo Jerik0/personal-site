7ea1a3426c3dd5e986115329d07deb159380b3ca
jerik0 <jcarrico1985@live.com>
2017-05-08 08:41:52 -0500

    started minor styling and layout. Changed first image.

--- logic.js
+++ logic.js
@@ -3,3 +3,15 @@ $(document).ready(function() {
 
 
 });
+
+//TODO: Features:
+//TODO: Portfolio page
+//TODO: Outside of Work page
+//TODO: Contact Me page
+
+//TODO: Small Features:
+//TODO: when scrolling, nav bar moves out of the way. when scrolling stops, nav bar returns.
+//TODO: projects will go full screen when screenshot is clicked on.
+//TODO: in nav bar, when item is clicked/tapped, that item gets wider and the other items get smaller.
+//TODO: when page loads, "My Name" text floats gently into place and becomes visible while doing so.
+
